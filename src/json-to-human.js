45042f7c1f5076c58b98cca2f3ef7841cf1c9b0d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-06-29 10:45:24 -0400

    feat(code): initial transformation of json to human format

--- src/json-to-human.js
+++ src/json-to-human.js
@@ -2,9 +2,32 @@
 
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const chalk = require('chalk')
+const checkMark = chalk.green('✓')
 
 function jsonToHuman (results) {
+  la(is.object(results), 'missing test results', results)
 
+  la(is.array(results.tests), 'missing all tests in', results)
+  la(is.array(results.passes), 'missing passed tests in', results)
+  la(is.array(results.failures), 'missing failed tests in', results)
+  la(is.array(results.pending), 'missing pending tests in', results)
+
+  var output = ''
+
+  results.passes.forEach((test) => {
+    output += `${checkMark} ${test.fullTitle}\n`
+  })
+
+  results.pending.forEach((test) => {
+    output += chalk.cyan(`- ${test.fullTitle}\n`)
+  })
+
+  results.failures.forEach((test) => {
+    output += chalk.red(`✗ ${test.fullTitle}\n`)
+  })
+
+  return output
 }
 
 module.exports = jsonToHuman
---
+++ test/json-human-reporter-spec.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/* global describe, it */
+const jhr = require('json-human-reporter')
+const relative = require('path').join.bind(null, __dirname)
+const la = require('lazy-ass')
+const is = require('check-more-types')
+
+describe('json-human-reporter', () => {
+  describe('works with files', () => {
+    it('handles all successful tests', () => {
+      const filename = relative('./example.json')
+      const output = jhr(filename)
+      la(is.unemptyString(output), 'converted output', output)
+      console.log(output)
+    })
+
+    it('handles some failures', () => {
+      const filename = relative('./fail.json')
+      const output = jhr(filename)
+      la(is.unemptyString(output), 'converted output', output)
+      console.log(output)
+    })
+  })
+})
